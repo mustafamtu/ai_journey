8024472e650546c060c4cd8012c9ecc31e2af00d
mustafamtu <mustafamtu3458@gmail.com>
2026-08-13 01:03:13 +0300

    Completed Multidimensional Arrays.

---
+++ intro.py
@@ -0,0 +1,18 @@
+# NumPy = Numerical Python
+
+import numpy as np
+
+# print(np.__version__)
+
+
+# my_list = [1,2,3,4]
+
+# my_list = my_list * 2   Equals to 1,2,3,4,1,2,3,4
+
+array = np.array([1,2,3,4])  
+
+array = array * 2
+
+print(array)
+
+print(type(array))
