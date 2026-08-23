3e262c97bb2bb7dd6082b6cadede1eee9c51e005
mustafamtu <mustafamtu3458@gmail.com>
2026-08-23 11:36:10 +0300

    Program kullanıma hazır hale getirildi.

--- 04_kisisel_asistan/chatbot-manager.py
+++ 04_kisisel_asistan/chatbot-manager.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+import sqlite3
+import uuid
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.chat_history import BaseChatMessageHistory
@@ -10,7 +12,7 @@ print("")
 load_dotenv()
 
 class ChatbotManager:
-    def __init__(self, model_name: str = "gemini-2.5-flash", db_path: str = "sqlite:///history.db"):
+    def __init__(self, model_name: str = "gemini-2.5-flash", db_path: str = "history.db"):
         """
         Chatbot Yöneticisini Başlatır.
 
@@ -20,9 +22,84 @@ class ChatbotManager:
         """
 
         self.model_name = model_name
-        self.db_path = db_path
+        self.db_file_path = db_path
+        self.connection_str = f"sqlite:///{db_path}"
         self.LLM = ChatGoogleGenerativeAI(model=self.model_name)
+
+        self._init_session_db()
+        
         self.conversation_chain = self._create_chain()
+
+
+    def create_session(self, user_id: str, title: str="Yeni Sohbet") -> str:
+        """
+        Beliri bir kullanıcı için yeni sohbet oturumu oluşturur.
+        Benzersiz bir session_id döner.
+        """
+
+        session_id = str(uuid.uuid4()) #örnek: 550e85 89890
+
+        conn = sqlite3.connect(self.db_file_path)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO chat_sessions (session_id, user_id, title) VALUES (?,?,?)", (session_id, user_id, title)),
+        conn.commit()
+        conn.close()
+
+        return session_id
+
+    def list_sessions(self, user_id: str):
+        """
+        Kullanıcının tüm sohbetlerini listeler. (id, başlık, tarih)
+        """
+
+        conn = sqlite3.connect(self.db_file_path)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT session_id, title, created_at FROM chat_sessions WHERE user_id=? ORDER BY created_at DESC",
+            (user_id,)
+        )      
+        rows = cursor.fetchall()
+        conn.close()
+
+        return [dict(row) for row in rows]
+
+    def update_session_title(self,user_id, session_id: str, new_title: str):
+        """
+        Sohbet Başlığını güncelle
+        """
+        conn = sqlite3.connect(self.db_file_path)
+        cursor = conn.cursor()                
+        cursor.execute(
+            "UPDATE chat_sessions SET title = ? WHERE session_id=?", (new_title,session_id)
+            
+        )      
+        conn.commit()
+        conn.close()
+        
+
+    def _init_session_db(self):
+        """
+        Sohbet başlıklarını ve sahiplerini tutan tabloyu oluşturur.
+        Langchain mesajları 'message_store' tablosunda tutar, 'chat_session' tablosunda oturum listesini
+        tutacağız.
+        """
+
+        conn = sqlite3.connect(self.db_file_path)
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS chat_sessions(
+                session_id TEXT PRIMARY KEY,
+                user_id TEXT,
+                title TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            """
+        )
+        conn.commit()
+        conn.close()
 
 
     def get_session_history(self,session_id: str) -> BaseChatMessageHistory:
@@ -32,7 +109,7 @@ class ChatbotManager:
                 
         return SQLChatMessageHistory(
             session_id=session_id,
-            connection=self.db_path
+            connection=self.connection_str,
         )
         
     def _create_chain(self):
@@ -53,6 +130,13 @@ class ChatbotManager:
             history_messages_key="geçmiş",
         )
 
+    def get_messages(self, session_id: str):
+        """
+        Bir oturumdaki mesaj geçmişini döner.
+        """
+        history = self._get_session_history(session_id)
+        return history.messages
+
     def chat(self, session_id:str, query: str) -> str:
         """
         Bot ile sohbet edebilmek için dışarıya açılan metod.
@@ -71,8 +155,20 @@ class ChatbotManager:
 if __name__ == "__main__":
     bot = ChatbotManager()
 
-    session_id = "mustafamtu"
+    user = "mustafamtu"
 
-    cevap = bot.chat(session_id, "benim adım ne ve ben ne öğreniyorum.")
+    sessions = bot.list_sessions(user)
 
-    print(f"Cevap: {cevap}")
+    if not sessions:
+        session_id = bot.create_session(user, title= "Python Dersleri Hakkında")
+        print(session_id)
+    else:
+        print(f"Sohbetler: {len(sessions)}")
+        session_id = sessions[0]['session_id'] #en son sohbet
+        print(session_id)
+
+    print(session_id)
+
+    # cevap = bot.chat(session_id, "benim adım ne ve ben ne öğreniyorum.")
+
+    # print(f"Cevap: {cevap}")
