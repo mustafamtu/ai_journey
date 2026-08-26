fe7a5b33089a118fec2cff5e2a5526c83d4ff062
mustafamtu <mustafamtu3458@gmail.com>
2026-08-26 17:24:42 +0300

    TAMAMLANDI FastAPI Bağlantısı.

--- 04_kisisel_asistan/api.py
+++ 04_kisisel_asistan/api.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI,HTTPException
 from chatbot import ChatbotManager
 
 class CreateSessionRequest(BaseModel):
-    user_id = str
+    user_id: str
     title: str = "Yeni Sohbet"
 
 class ChatRequest(BaseModel):
@@ -15,4 +15,38 @@ class MessageResponse(BaseModel):
     content: str
 
 app = FastAPI(title="Chatbot API")
-bot = ChatbotManager
+bot = ChatbotManager()
+
+@app.post("/sessions/create")
+def create_session(request: CreateSessionRequest):
+    try:
+        session_id = bot.create_session(request.user_id, request.title)
+        return {"session_id": session_id}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/sessions/{user_id}")
+def list_sessions(user_id: str):
+    return bot.list_sessions(user_id)
+
+
+
+@app.get("/history/{session_id}")
+def get_history(session_id: str):
+    messages = bot.get_messages(session_id)
+
+    result = []
+
+    for m in messages:
+        role = "user" if m.type=="human" else "assistant"
+        result.append({"role":role, "content": m.content})
+    return result
+
+@app.post("/chat")
+def chat_endpoint(request: ChatRequest):
+    cevap = bot.chat(request.session_id, request.query)
+    return {"response": cevap}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
